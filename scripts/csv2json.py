07d5001ff96497e07e91e69dd8a9cdfc5f6e7068
Gilmore Davidson <gilmoreorless@gmail.com>
2016-04-15 07:52:04 +1000

    Reformatted JSON to be comma-first to avoid bugs
    It prevents trailing-comma situations (generated by conversion while file streaming),
    which JSON parsers choke on.

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -4,17 +4,19 @@ import sys
 import csv
 import json
 
-iata_re = re.compile('^[a-zA-Z0-9]{3}$')
+iata_re = re.compile('^[A-Z0-9]{3}$')
 reader = csv.DictReader(sys.stdin)
 outfile = sys.stdout
 
 outfile.writelines('{\n')
+joiner = ' '
 for line in reader:
     if iata_re.match(line['iata_code']):
-        prefix = '\t"%s": ' % (line['iata_code'].upper())
+        prefix = '  %s "%s": ' % (joiner, line['iata_code'].upper())
         obj = [line['name']]
         if line['municipality'] != '':
             obj.append(line['municipality'])
         data = json.dumps(obj)
-        outfile.writelines(prefix + data + ',\n')
+        outfile.writelines(prefix + data + '\n')
+        joiner = ','
 outfile.writelines('}\n')
