da6bfd76d0def76962d2e26d64b231d36e4f0955
Gilmore Davidson <gilmoreorless@gmail.com>
2017-03-10 17:55:09 +1100

    Updated airport data and removed junk entries

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -5,6 +5,7 @@ import csv
 import json
 
 iata_re = re.compile('^[A-Z0-9]{3}$')
+exclude_re = re.compile('\[Duplicate\]|Erase Me')
 reader = csv.DictReader(sys.stdin)
 outfile = sys.stdout
 
@@ -13,6 +14,8 @@ joiner = ' '
 for line in reader:
     if iata_re.match(line['iata_code']):
         prefix = '  %s "%s": ' % (joiner, line['iata_code'].upper())
+        if exclude_re.match(line['name']):
+            continue
         obj = [line['name']]
         if line['municipality'] != '':
             obj.append(line['municipality'])
